40e130a18ea440e79238d6302241d009ded942f6
Wessell <86524864+WessellUrdata@users.noreply.github.com>
2025-07-10 20:08:18 +0800

    feat(fish): added fish config
    although I'm probably moving to zsh soon

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,12 @@
+if status is-interactive
+    alias ls='ls -lah --color=auto'
+    alias q='exit'
+    alias sc='systemctl'
+    alias p='paru'
+
+    export EDITOR=nvim
+    export SUDO_EDITOR=nvim
+end
+
+function fish_greeting
+end
